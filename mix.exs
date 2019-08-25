71e7f87f88801b194f570a7e64e8094e09bbdfbb
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2019-08-25 09:33:43 +0800

    upgrade to elixir 1.9 and plug_cowboy 2.1 and latest phoenix_liveview

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LiveViewTodos.MixProject do
     [
       app: :live_view_todos,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -43,7 +43,7 @@ defmodule LiveViewTodos.MixProject do
       {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.1"}
     ]
   end
 
