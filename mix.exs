4a3e077b2c3a693ceda625cbf5eee82beb197733
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2019-08-26 12:20:29 +0800

    upgrade phoenix.liveview to hex package version 0.1.0

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule LiveViewTodos.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:phoenix_live_view, "~> 0.1"},
       {:phoenix_pubsub_redis, "~> 2.1.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
