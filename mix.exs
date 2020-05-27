847e57c4f6b5e9c964438974ae219a7eaa86f3fd
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2020-05-27 16:04:51 +0800

    upgrade to phoenix 1.5.3 live_view 0.13.1 and npm packages

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule LiveViewTodos.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.12.1", override: true},
+      {:phoenix_live_view, "~> 0.13.0", override: true},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_pubsub_redis, "~> 3.0"},
       {:gettext, "~> 0.11"},
